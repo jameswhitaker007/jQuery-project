5c50fadc7dd4f989400949b7b7a629d96742ee9c
James Whitaker <jwhitaker0007@gmail.com>
2023-10-01 08:52:29 -0400

    "Initial upload"

---
+++ js/main.js
@@ -0,0 +1,176 @@
+//var fields = ["username", "password"];
+/*function loginApi(e) {
+  e.preventDefault();
+  console.log(e);
+  $(".wrapper").show();
+  var loginForm = $(".loginForm")[0];
+  console.log(loginForm);
+  //fields.forEach((field) => validateFields(field));
+  if (loginForm.checkValidity() === false) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(".wrapper").hide();
+    return;
+  }
+
+  console.log($("#username").val());
+
+  $.post("https://dummyjson.com/auth/login", {
+    username: $("#username").val(),
+    password: $("#password").val(),
+  })
+    .done(function (data) {
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      window.location.replace("./subpages/product.html");
+    })
+    .fail(function (data) {
+      console.error(data);
+    })
+    .always(function () {
+      console.log("Finished");
+    });
+
+  $(".wrapper").hide();
+}*/
+
+/*function validateFields(field) {
+  var tempEle = $("#" + field);
+  console.log(tempEle);
+  if (tempEle.val() == "") {
+    tempEle.removeClass("is-valid").addClass("is-invalid");
+  } else {
+    tempEle.removeClass("is-invalid").addClass("is-valid");
+  }
+}*/
+
+/*$("document").ready(function () {
+  $("#submitBtn").click(loginApi);
+});*/
+
+//window.history.pushState("./index.html");
+
+/*(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+  console.log(forms);
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    console.log(form);
+    form.addEventListener(
+      "submit",
+      function (event) {
+        $(".wrapper").show();
+        if (!form.checkValidity()) {
+          console.log("error");
+          event.preventDefault();
+          event.stopPropagation();
+          $(".wrapper").hide();
+          return;
+        }
+        form.classList.add("was-validated");
+        event.preventDefault();
+        $.post("https://dummyjson.com/auth/login", {
+          username: $("#username").val(),
+          password: $("#password").val(),
+        })
+          .done(function (data) {
+            localStorage.setItem("token", data.token);
+
+            window.location.replace("./subpages/product.html");
+          })
+          .fail(function (data) {
+            console.error(data);
+          })
+          .always(function () {
+            console.log("Finished");
+          });
+
+        $(".wrapper").hide();
+      },
+      false
+    );
+  });
+})();*/
+
+/*$("document").ready(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+  //console.log(forms);
+
+  var newForm = $('#form');
+  console.log(newForm);
+  newForm[0].addEventListener('submit', function() {
+    console.log('hi');
+  })
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    //console.log(form);
+    form.addEventListener(
+      "submit",
+      function (event) {
+        $(".wrapper").show();
+        if (!form.checkValidity()) {
+          console.log("error");
+          event.preventDefault();
+          event.stopPropagation();
+          $(".wrapper").hide();
+          return;
+        }
+        form.classList.add("was-validated");
+        event.preventDefault();
+        $.post("https://dummyjson.com/auth/login", {
+          username: $("#username").val(),
+          password: $("#password").val(),
+        })
+          .done(function (data) {
+            localStorage.setItem("token", data.token);
+
+            window.location.replace("./subpages/product.html");
+          })
+          .fail(function (data) {
+            console.error(data);
+          })
+          .always(function () {
+            console.log("Finished");
+          });
+
+        $(".wrapper").hide();
+      },
+      false
+    );
+  });
+});*/
+
+$("document").ready(function () {
+  $("#form")[0].addEventListener("submit", function (event) {
+    event.preventDefault();
+    $(".wrapper").show();
+    form.classList.add("was-validated");
+    if (!form.checkValidity()) {
+      event.stopPropagation();
+      $(".wrapper").hide();
+      return;
+    }
+    $.post("https://dummyjson.com/auth/login", {
+      username: $("#username").val(),
+      password: $("#password").val(),
+    })
+      .done(function (data) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("name", data.firstName + " " + data.lastName);
+        console.log(data);
+        window.location.replace("./sub_pages/product.html");
+      })
+      .fail(function (error) {
+        console.error(error.responseJSON.message);
+      });
+    $(".wrapper").hide();
+  });
+});
---
+++ js/product.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+  var token = localStorage.getItem("token");
+  if (token == undefined || token == "") {
+    window.location.replace("../index.html");
+  }
+
+  $("#user").text("Hi,\n" + localStorage.getItem("name"));
+
+  $.ajax({
+    type: "GET",
+    url: "https://dummyjson.com/auth/products",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      //Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+    success: function (data) {
+      console.log(data);
+      //$("#product").text(JSON.stringify(data.products));
+      appendProductCards(data.products);
+      $(".wrapper").hide();
+    },
+    error: function (error) {
+      console.error(error);
+      if (error.status === 403) {
+        var messageObj = JSON.parse(error.responseText);
+        alert(messageObj.message);
+        window.location.replace("../index.html");
+      }
+      $(".wrapper").hide();
+    },
+    dataType: "json",
+  });
+
+  const toastLiveExample = document.getElementById("liveToast");
+  const toast = new bootstrap.Toast(toastLiveExample);
+  toast.show();
+
+  $("#log-out").click(logout);
+});
+
+function appendProductCards(products) {
+  $.each(products, function (index, obj) {
+    $("#product").append(`<div class="col-md-6 col-lg-4 gy-3">
+    <div class="card h-100" >
+      <div class="d-flex p-3">
+        <p class="lead">${obj.title}</p>
+      </div>
+      <img src="${obj.images[0]}" alt="${obj.title}" class="img-responsive"/>
+      <div class="card-body">
+        <p class="small">
+          <a href="#" class="text-muted">${obj.category}</a>
+        </p>
+        <div class="d-flex justify-content-between">
+        <h5>${obj.brand}</h5>
+        <h5 class="text-danger">$${obj.price}.00</h5>
+      </div>
+        <div class="d-flex justify-content-between">
+          <p class="text-muted">Rating :</p>
+          <div class="ms-auto text-warning">
+            ${addStars(obj.rating)}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`);
+  });
+}
+
+/*function addStars(stars) {
+  var wholeStars = Math.floor(stars);
+  var decimalValue = stars.toString().indexOf(".");
+  var value = stars.toString().substring(decimalValue + 1);
+  var halfStar = value >= 50 ? 1 : 0;
+  var emptyStars = Math.floor(5 - wholeStars - halfStar);
+  var result = "";
+  if (wholeStars > 0) {
+    for (i = 0; i < wholeStars; i++) {
+      result += '<i class="fa-solid fa-star"></i>';
+    }
+  }
+  if (halfStar == 1) {
+    result += '<i class="fa-solid fa-star-half-stroke"></i>';
+  }
+  if (emptyStars > 0) {
+    for (j = 0; j < emptyStars; j++) {
+      result += '<i class="fa-regular fa-star"></i>';
+    }
+  }
+  return result;
+}*/
+
+function addStars(stars) {
+  var fullStars = Math.floor(stars);
+  var decimalValue = stars.toString().indexOf(".");
+  var halfStar = stars.toString().substring(decimalValue + 1) >= 50 ? 1 : 0;
+  var emptyStars = 5 - fullStars - halfStar;
+  var result = "";
+  if (fullStars > 0) {
+    for (i = 0; i < fullStars; i++) {
+      result += '<i class="fa-solid fa-star"></i>';
+    }
+  }
+  if (halfStar == 1) {
+    result += '<i class="fa-solid fa-star-half-stroke"></i>';
+  }
+  if (emptyStars > 0) {
+    for (j = 0; j < emptyStars; j++) {
+      result += '<i class="fa-regular fa-star"></i>';
+    }
+  }
+  return result;
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("name");
+  window.location.replace("../index.html");
+}
